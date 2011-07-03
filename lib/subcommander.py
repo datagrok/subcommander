d1148414a674377884168d0d67e1d8f4a7dee662
Michael F. Lamb <mike@datagrok.org>
2011-07-03 16:16:08 -0400

    Add stub python implementation

---
+++ lib/subcommander.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python
+
+'''subcommander.py
+
+A python implementation of subcommander.sh. It should work almost exactly the
+same way but perhaps enable the context file to contain python code.
+
+'''
+
+# FIXME TODO LOLZ
+
+if __name__ == '__main__':
+    raise NotImplementedError('This is a work in progress...')
